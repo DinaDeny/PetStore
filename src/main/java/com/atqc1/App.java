16c1cbba2868aa3000ef21bd89fd5e11a0f071c8
DinaDeny <denisenkodina20@gmail.com>
2020-04-26 20:50:31 +0300

    update10

--- src/main/java/com/atqc1/App.java
+++ src/main/java/com/atqc1/App.java
@@ -1,13 +1,15 @@
 package com.atqc1;
 
+import java.util.ArrayList;
+
 import static com.atqc1.Breed.DOG;
 import static com.atqc1.Color.BLACK;
 import static com.atqc1.Color.GREEN;
 
 public class App {
-    public static void main (String[] args){
+    public static void main(String[] args) {
 
-        Pet dog = new Dog ("$100", "large Labrador");
+        Pet dog = new Dog("$100", "large Labrador");
         dog.walk();
         dog.setColor("BLACK");
         dog.sell(dog.getPrice(), "DOG");
@@ -23,5 +25,17 @@ public class App {
 
         System.out.println(Pet.soldPetCount);
 
+        ArrayList<String> soldPets = new ArrayList<>();
+        soldPets.add("ELEPHANT");
+        soldPets.add("CAT");
+        soldPets.add("PARROT");
+
+        soldPets.add(1, "DOG");
+        soldPets.remove(0);
+
+        System.out.println("List of pets that we sold yesterday: ");
+        for (String pet : soldPets) {
+            System.out.println(pet);
+        }
     }
 }
